cf2c3410663e583f95e13ca570be6bb043ef7664
leobarna <leonoradli94@gmail.com>
2021-07-23 14:23:44 +0200

    add webpaackconfig.js loader plugin

--- web-client/src/js/components/navigationBar.jsx
+++ web-client/src/js/components/navigationBar.jsx
@@ -1,10 +1,16 @@
 import * as React from "react";
 import {Link} from "react-router-dom";
+import logo from "./assets/logo-transparente.png";
 
 export const NavigationBar = () => (
     <nav className={"navigationBar"}>
+        <div className={"logoImg"}>
+            <a><Link to={"/app"}> <img src={logo} alt={""} /> </Link></a>
+        </div>
+        <div className={"itemsLinks"}>
         <Link to={"/quienesSomos"}>Quienes Somos </Link>
         <Link to={"/experiencias"}>Experiencias </Link>
         <Link to={"/reservas"}>Reservas </Link>
+        </div>
     </nav>
 )
