1d564539340c305913e055904ef36d64636d7292
leobarna <leonoradli94@gmail.com>
2021-09-08 20:18:13 +0200

    Updates

--- src/main/java/net/econotravel/econotravelapp/repositories/ExperienceRepository.java
+++
@@ -1,8 +0,0 @@
-package net.econotravel.econotravelapp.repositories;
-
-import net.econotravel.econotravelapp.domain.Experience;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ExperienceRepository extends JpaRepository<Experience, Long>{}
--- web-client/src/js/components/footer.jsx
+++
@@ -1,9 +0,0 @@
-import * as React from "react";
-
-export const Footer = () => {
-    return (
-        <div className={"footer"}>
-            <p>Econotravel Agency © 2021. All rights reserved.</p>
-        </div>
-    )
-}
